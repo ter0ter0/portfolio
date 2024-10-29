2919586e324b97063475e1d958642ca3143335e8
NeitoKomiyama <neito.komi11@gmail.com>
2024-10-29 12:50:25 +0900

    #115　ユーザー詳細：修正

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -18,11 +18,11 @@
         </aside>
         <div class="col-sm-8">
             <ul class="nav nav-tabs nav-justified mb-3">
-                <li class="nav-item"><a href="" class="nav-link {{ Request::is() ? 'active' : '' }}">タイムライン</a></li>
+                <li class="nav-item"><a href="" class="nav-link {{ Request::is('users/' . $user->id) ? 'active' : '' }}">タイムライン</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">フォロー中</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">フォロワー</a></li>
             </ul>
-            {{-- @include('posts.posts', ['user' => $user, 'posts' => $posts]) --}}
+            @include('posts.posts', ['user' => $user, 'posts' => $posts])
         </div>
     </div>
 @endsection
