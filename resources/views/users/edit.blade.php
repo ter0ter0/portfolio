436de00c865b2aa6b7884557a09990f945803072
takeuchi <panicdepanic@gmail.com>
2024-11-07 08:47:30 +0900

    #125 ユーザー退会（竹内)

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <h2 class="mt-5 mb-3">ユーザ情報を編集する</h2>
-    <form method="POST" action="">
+    <form method="" action="{ route('users.edit')}}">
         <input type="hidden" name="id" value="" />
         <div class="form-group">
             <label for="name">ユーザ名</label>
