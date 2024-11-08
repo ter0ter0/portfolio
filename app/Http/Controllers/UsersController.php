3ad68f565e3a96a68b2e3588de5bf2311ff55ab1
takeuchi <panicdepanic@gmail.com>
2024-11-08 15:19:06 +0900

    #125 ユーザー退会（竹内）

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,7 +22,7 @@ class UsersController extends Controller
     public function destroy($id)
     {
         $user = User::findOrFail($id);
-        if($user->id === \Auth::id()){
+        if ($user->id === \Auth::id()) {
             $user->delete();
         }
         return redirect()->route('post.index');
---
+++ database/migrations/2024_11_08_144119_create_favorites_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use League\CommonMark\Reference\Reference;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigIncrements('user_id');
+            $table->bigIncrements('post_id');
+            $table->timestamps();
+            // 外部キー制約
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->unique(['user_id','post_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
