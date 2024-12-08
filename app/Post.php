1a806d24a1b76277502a8085235d54a5ef874da8
NeitoKomiyama <neito.komi11@gmail.com>
2024-12-08 16:27:54 +0900

    #1348 タグ機能：レビュー指摘事項の修正

--- app/Post.php
+++ app/Post.php
@@ -34,6 +34,6 @@ class Post extends Model
 
     public function tags()
     {
-        return $this->belongsToMany(Tag::class);
+        return $this->belongsToMany(Tag::class)->withTimestamps();
     }
 }
