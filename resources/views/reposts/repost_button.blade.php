fff7d09773b84d56d7ed4531a62c7283e6e5c52f
takeuchi <panicdepanic@gmail.com>
2024-12-15 13:13:17 +0900

    途中

--- resources/views/reposts/repost_button.blade.php
+++ resources/views/reposts/repost_button.blade.php
@@ -4,18 +4,18 @@
         <form method="POST" action="{{ route('repost.delete', $post->id) }}#post-{{ $post->id }}"class="d-inline-block" data-toggle="tooltip" title="リポストを取り消す">
             @csrf
             @method('DELETE')
-            <button type="submit" class="btn m-0 p-1 shadow-none"><i class="bi bi-arrow-repeat"></i></button>
+            <button type="submit" class="btn m-0 p-1 shadow-none"><i class="bi bi-arrow-repeat text-success"></i></button>
         </form>
     @else
         <form method="POST" action="{{ route('repost.store', $post->id) }}#post-{{ $post->id }}" class="d-inline-block" data-toggle="tooltip" title="リポスト">
             @csrf
-            <button type="submit" class="btn m-0 p-1 shadow-none"><i class="bi bi-arrow-repeat text-success"></i></button>
+            <button type="submit" class="btn m-0 p-1 shadow-none"><i class="bi bi-arrow-repeat"></i></button>
         </form>
     @endif
 @else
-    <a href="{{ route('login') }}" class="btn m-0 p-1 shadow-none" data-toggle="tooltip" title="ログインしてリポスト"><i class="bi bi-heart"></i></a>
+    <a href="{{ route('login') }}" class="btn m-0 p-1 shadow-none" data-toggle="tooltip" title="ログインしてリポスト"><i class="bi bi-arrow-repeat"></i></a>
 @endif
-<span id="favorite-count-{{ $post->id }}" class="badge badge-pill">{{ $post->repostedPosts()->count() }}</span>
+<span id="repost-count-{{ $post->id }}" class="badge badge-pill">{{ $post->repostedPosts()->count() }}</span>
 
 <script>
     // ツールチップ初期化処理
